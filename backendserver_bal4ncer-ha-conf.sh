d86a1bd2a28f5fdbbe4ee029811029b26256342c
mf3d3r <32132926+mf3d3r@users.noreply.github.com>
2018-01-02 15:56:05 +0100

    test2

---
+++ backendserver_bal4ncer-ha-conf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo chmod a+w /etc/haproxy/haproxy.cfg
+echo "frontend http_front" >> /etc/haproxy/haproxy.cfg;
+echo "bind *:80" >> /etc/haproxy/haproxy.cfg
+echo "stats uri /haproxy?stats" >> /etc/haproxy/haproxy.cfg
+echo "default_backend http_back" >> /etc/haproxy/haproxy.cfg
+echo "backend http_back" >> /etc/haproxy/haproxy.cfg
+echo "balance roundrobin" >> /etc/haproxy/haproxy.cfg
+echo "server backend-server1 192.168.33.6:80 check" >> /etc/haproxy/haproxy.cfg
+sudo service haproxy restart
+   
