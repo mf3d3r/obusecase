2e5e8ac2389d4f3e192f37608732545647bc3b5d
mf3d3r <32132926+mf3d3r@users.noreply.github.com>
2018-01-02 20:58:56 +0100

    erwer

--- bal4ncer-ha-install.sh
+++ bal4ncer-ha-install.sh
@@ -4,12 +4,12 @@ sudo add-apt-repository ppa:vbernat/haproxy-1.7
 sudo apt update
 sudo apt install -y haproxy
 CONFIG="/etc/haproxy/haproxy.cfg"
-sudo chmod a+w CONFIG
-echo "frontend http_front" >> CONFIG
+sudo chmod a+w $CONFIG
+echo "frontend http_front" >> $CONFIG
 echo "bind *:80" >> CONFIG
-echo "stats uri /haproxy?stats" >> CONFIG
-echo "default_backend http_back" >> CONFIG
-echo "backend http_back" >> CONFIG
-echo "balance roundrobin" >> CONFIG
-echo "server backend-server1 192.168.33.6:80 check" >> CONFIG
+echo "stats uri /haproxy?stats" >> $CONFIG
+echo "default_backend http_back" >> $CONFIG
+echo "backend http_back" >> $CONFIG
+echo "balance roundrobin" >> $CONFIG
+echo "server backend-server1 192.168.33.6:80 check" >> $CONFIG
 sudo service haproxy restart
