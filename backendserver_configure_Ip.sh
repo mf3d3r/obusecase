a991034a1f58061eb18cbfbdb052d54cb272b114
mf3d3r <32132926+mf3d3r@users.noreply.github.com>
2018-01-03 14:16:35 +0100

    neuer test

--- backendserver_configure_Ip.sh
+++ backendserver_configure_Ip.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+touch /tmp/ip-private
+echo "backendserver_private_Ip: $backendserver_private_Ip" >> /tmp/ip-private
 CONFIG="/etc/haproxy/haproxy.cfg"
-echo "server backend-server1 $server_private_floatingIp check" >> $CONFIG
+echo "server backend-server1 $backendserver_private_Ip check" >> $CONFIG
 sudo service haproxy restart
    
