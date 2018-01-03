b63fc61aaeb065aec6508efab9208ffc6080260e
mf3d3r <32132926+mf3d3r@users.noreply.github.com>
2018-01-03 14:49:02 +0100

    insert private ip to HA

--- server_configure.sh
+++ server_configure.sh
@@ -2,6 +2,6 @@
 touch /tmp/ip-private
 echo "server_private_Ip: $server_private" >> /tmp/ip-private
 CONFIG="/etc/haproxy/haproxy.cfg"
-echo "server backend-server1 $server_private check" >> $CONFIG
+echo "server backend-server1 ${server_private}:80 check" >> $CONFIG
 sudo service haproxy restart
    
