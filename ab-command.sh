b2722fe68263d4eee3209efcd2e5892d548cb6fa
mf3d3r <32132926+mf3d3r@users.noreply.github.com>
2018-01-04 13:38:35 +0100

    finaltest1

---
+++ ab-command.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ab -n 100000 -c 1000  http://192.168.33.5/testpage.html
+
--- install-apache.sh
+++ install-apache.sh
@@ -2,4 +2,14 @@
 
 sudo apt-get update && sudo apt-get install -y apache2
 /etc/init.d/apache2 restart
-
+touch /var/www/html/testpage.html
+TESTPAGE="/var/www/html/testpage.html"
+echo "<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">" >> $TESTPAGE
+echo "<html>" >> $TESTPAGE
+echo "<head>" >> $TESTPAGE
+echo "<title>Webserver test</title>" >> $TESTPAGE
+echo "</head>" >> $TESTPAGE
+echo "<body>" >> $TESTPAGE
+echo "This is a webserver test page." >> $TESTPAGE
+echo "</body>" >> $TESTPAGE
+echo "</html>" >> $TESTPAGE
