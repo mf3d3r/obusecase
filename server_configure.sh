5e50bafa8a66f045af5d69a4480abac5dc697bbb
mf3d3r <32132926+mf3d3r@users.noreply.github.com>
2018-01-05 15:12:44 +0100

    counter

--- server_configure.sh
+++ server_configure.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 touch /tmp/counter
-echo "server_counter: $server_counter“ >> /tmp/counter
+echo "server_counter: $server_counter" >> /tmp/counter
 CONFIG="/etc/haproxy/haproxy.cfg"
-echo "server backend-server-${server_counter} ${server_private}:80 check" >> $CONFIG
+echo "server backend-server ${server_private}:80 check" >> $CONFIG
 sudo service haproxy restart
-   
